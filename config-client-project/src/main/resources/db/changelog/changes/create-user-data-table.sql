3aa532e823933a4be9bb9f40829f8a3a20f1de94
k1den <nedoswimmer@gmail.com>
2025-07-24 13:06:45 +0300

    Добавил создание таблицы через Liquibase

---
+++ config-client-project/src/main/resources/db/changelog/changes/create-user-data-table.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset k1den:1
+CREATE TABLE user_data (
+                           id BIGSERIAL PRIMARY KEY,
+                           name VARCHAR(255) NOT NULL,
+                           email VARCHAR(255) NOT NULL,
+                           message VARCHAR(255),
+                           created_at TIMESTAMP
+);
