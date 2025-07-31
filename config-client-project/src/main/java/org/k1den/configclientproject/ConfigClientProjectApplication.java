987dd68484b8a3ccf304e3382ba2aa11f1005e2a
k1den <nedoswimmer@gmail.com>
2025-07-31 14:22:54 +0300

    Попытка с jasypt V1.0

--- config-client-project/src/main/java/org/k1den/configclientproject/ConfigClientProjectApplication.java
+++ config-client-project/src/main/java/org/k1den/configclientproject/ConfigClientProjectApplication.java
@@ -1,9 +1,11 @@
 package org.k1den.configclientproject;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ConfigClientProjectApplication {
 
     public static void main(String[] args) {
