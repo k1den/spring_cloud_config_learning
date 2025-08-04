71769067f5e7dae79a4f82f506bb7b59cbae89ad
k1den <nedoswimmer@gmail.com>
2025-08-04 18:50:22 +0300

    Понизил версию spring boot и spring cloud config

--- config-client-project/src/main/java/org/k1den/configclientproject/JasyptEncryptionExample.java
+++ config-client-project/src/main/java/org/k1den/configclientproject/JasyptEncryptionExample.java
@@ -6,20 +6,17 @@ import org.jasypt.iv.RandomIvGenerator;
 public class JasyptEncryptionExample {
 
     public static void main(String[] args) {
-        // Создаем encryptor с теми же параметрами, что и в вашем приложении
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword("client1"); // Пароль для шифрования (должен совпадать с jasypt.encryptor.password)
+        encryptor.setPassword("client1");
         encryptor.setAlgorithm("PBEWithMD5AndDES");
         encryptor.setIvGenerator(new RandomIvGenerator());
 
-        // Пароль, который нужно зашифровать
-        String rawPassword = "client1"; // Замените на реальный пароль для config server
+        String rawPassword = "client1";
         String encryptedPassword = encryptor.encrypt(rawPassword);
 
         System.out.println("Raw password: " + rawPassword);
         System.out.println("Encrypted password: ENC(" + encryptedPassword + ")");
 
-        // Для проверки - дешифруем обратно
         String decryptedPassword = encryptor.decrypt(encryptedPassword);
         System.out.println("Decrypted password: " + decryptedPassword);
     }
